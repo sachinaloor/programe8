1c86cd21ca6262ce59502ac53313807042236512
sachinaloor <sachinaloor48@gmail.com>
2025-12-01 15:03:38 +0530

    save

---
+++ employe3.py
@@ -0,0 +1,18 @@
+import pytest
+def employee_info(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
+    )
+    return result
+
+if __name__ == "__main__":
+   
+        name = "Alice\n"
+        emp_id = "E101\n"
+        department = "IT\n"
+        salary = 55000
+
+        print(employee_info(name, emp_id, department, salary))
---
+++ test_employe3.py
@@ -0,0 +1,10 @@
+from employee import employee_info
+
+def test_employee_info():
+    
+    expected_output = (
+        "Employee Name: Bouna\n"
+        "Employee ID: 101\n"
+        "Department: IT\n"
+        "Salary: 30000"
+    )
